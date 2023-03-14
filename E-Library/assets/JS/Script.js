f00dbf2b48953bed839afcc10f5767f2c652b50d
alishbaidrees <alishbaid456@gmail.com>
2023-03-15 01:59:30 +0500

    e-library assignment project

---
+++ E-Library/assets/JS/Script.js
@@ -0,0 +1,39 @@
+
+/* Current time */
+
+
+
+/*   pre loader*/
+
+
+	$(document).ready(function() {
+  setTimeout(function() {
+    $('#container').addClass('loaded');
+    // Once the container has finished, the scroll appears
+    if ($('#container').hasClass('loaded')) {
+      // It is so that once the container is gone, the entire preloader section is deleted
+      $('#preloader').delay(500).queue(function() {
+        $(this).remove();
+      });}
+  }, 3000);});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
